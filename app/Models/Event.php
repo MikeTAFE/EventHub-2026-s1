cc36a649ef8ad4c0a2081e3bc247a7319886ea69
Mike Kirkwood-Smith <michael.kirkwood-smith3@tafensw.edu.au>
2026-05-21 21:32:37 +1000

    Added `$fillable` property to Event model to allow mass assignment of properties

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Event extends Model
 {
+    // Allow mass assignment (multiple property values at once)
+    protected $fillable = ['name', 'description', 'image', 'featured', 'category_id'];
+
     /**
      * Get category for the event.
      *
