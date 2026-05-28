f18d87ba8bde7749d3781b95be331112e861b0fc
Mike Kirkwood-Smith <michael.kirkwood-smith3@tafensw.edu.au>
2026-05-28 21:47:21 +1000

    Updated event show view to have slightly better styling

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -2,17 +2,23 @@
 @section('title', 'Event details')
 @section('content')
 
-  <h1 class="text-4xl text-heading">Event details</h1>
+  <section class="bg-white">
+    <div class="max-w-7xl mx-auto px-6 py-16">
 
-  <table>
-    <tr>
-      <th>Name:</th>
-      <td>{{ $event["name"] }}</td>
-    </tr>
-    <tr>
-      <th>City:</th>
-      <td>{{ $event["city"] }}</td>
-    </tr>
-  </table>
+      <h1 class="h1">Event details</h1>
+
+      <table class="table-auto border-collapse border border-slate-400">
+        <tr>
+          <th>Name:</th>
+          <td>{{ $event["name"] }}</td>
+        </tr>
+        <tr>
+          <th>City:</th>
+          <td>{{ $event["city"] }}</td>
+        </tr>
+      </table>
+
+    </div>
+  </section>
 
 @endsection
