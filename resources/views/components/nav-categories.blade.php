e5ae65534c999ab5ba98bd017c8b8142cfbc55e4
Mike Kirkwood-Smith <michael.kirkwood-smith3@tafensw.edu.au>
2026-06-01 21:33:26 +1000

    Fixed category nav styling

--- resources/views/components/nav-categories.blade.php
+++ resources/views/components/nav-categories.blade.php
@@ -1,9 +1,7 @@
-<div>
-    <ul class="flex gap-4">
-        @foreach ($categories as $category)
-            <li class="bg-white hover:bg-blue-100 p-6 rounded-xl shadow text-center">
-                <a href="{{ route("categories.show", $category) }}">{{ $category->name }}</a>
-            </li>
-        @endforeach
-    </ul>
-</div>
+<ul class="flex gap-4 my-3">
+    @foreach ($categories as $category)
+        <li class="bg-white hover:bg-blue-100 rounded-xl shadow text-center">
+            <a href="{{ route("categories.show", $category) }}" class="inline-block py-3 px-5">{{ $category->name }}</a>
+        </li>
+    @endforeach
+</ul>
